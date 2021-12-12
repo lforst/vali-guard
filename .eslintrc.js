98e57018dd99eb27416654a0740e80b3e3634054
Luca Forstner <luca.forstner@gmail.com>
2021-12-12 17:02:45 +0000

    Initial Setup

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+// @ts-check
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+    root: true,
+    ignorePatterns: ['dist/**/*'],
+    overrides: [
+        {
+            parser: '@typescript-eslint/parser',
+            plugins: ['@typescript-eslint', 'jest'],
+            env: {
+                'jest/globals': true
+            },
+            files: ['*.ts'],
+            parserOptions: {
+                project: ['tsconfig.json'],
+                createDefaultProgram: true
+            },
+            extends: [
+                'eslint:recommended',
+                'plugin:jest/recommended',
+                'plugin:jest/style',
+                'plugin:@typescript-eslint/recommended',
+                'prettier'
+            ],
+            settings: {
+                jest: {
+                    version: require('jest/package.json').version
+                }
+            }
+        }
+    ],
+    rules: {
+        'no-console': 'error'
+    }
+};
