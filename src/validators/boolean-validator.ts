1b03fd88bde718fb42dffab297c2f46cfb966a0a
Luca Forstner <luca.forstner@gmail.com>
2022-02-28 09:58:12 +0000

    Add boolean validator

---
+++ src/validators/boolean-validator.ts
@@ -0,0 +1,16 @@
+import { ValidationDiagnostics } from '../types';
+import { BaseValidator } from './base-validator';
+
+export class BooleanValidator extends BaseValidator<boolean> {
+    validate(subject: unknown, diagnostics?: ValidationDiagnostics): subject is boolean {
+        const validationResult = typeof subject === 'boolean';
+
+        if (!validationResult && diagnostics) {
+            Object.assign(diagnostics, {
+                error: 'not boolean'
+            });
+        }
+
+        return validationResult;
+    }
+}
