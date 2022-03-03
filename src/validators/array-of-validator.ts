be6b11c3081fcd385243de5f4688a0833e37e2f9
Luca Forstner <luca.forstner@gmail.com>
2022-03-03 09:18:56 +0000

    Refactor ArrayOfValidator

--- src/validators/array-of-validator.ts
+++ src/validators/array-of-validator.ts
@@ -2,27 +2,11 @@ import { Validatable, ValidatableType, ValidationDiagnostics } from '../types';
 import { isArray } from '../utils/is-array';
 import { BaseValidator } from './base-validator';
 
-export interface ArrayOfValidatorOptions {
-    maxLength: number;
-    minLength: number;
-}
-
-const defaultOptions: ArrayOfValidatorOptions = {
-    maxLength: Infinity,
-    minLength: -Infinity,
-};
-
 export class ArrayOfValidator<T extends Validatable<unknown>> extends BaseValidator<
     ValidatableType<T>[]
 > {
-    private readonly options: ArrayOfValidatorOptions;
-
-    constructor(private readonly validator: T, options: Partial<ArrayOfValidatorOptions> = {}) {
+    constructor(private readonly validator: T) {
         super();
-        this.options = {
-            ...defaultOptions,
-            ...options,
-        };
     }
 
     validate(
@@ -33,24 +17,6 @@ export class ArrayOfValidator<T extends Validatable<unknown>> extends BaseValida
             if (diagnostics) {
                 Object.assign(diagnostics, {
                     error: 'not array',
-                });
-            }
-            return false;
-        }
-
-        if (subject.length > this.options.maxLength) {
-            if (diagnostics) {
-                Object.assign(diagnostics, {
-                    error: `array length exceeds maximum (${this.options.maxLength})`,
-                });
-            }
-            return false;
-        }
-
-        if (subject.length < this.options.minLength) {
-            if (diagnostics) {
-                Object.assign(diagnostics, {
-                    error: `array length below minimum (${this.options.minLength})`,
                 });
             }
             return false;
@@ -75,17 +41,81 @@ export class ArrayOfValidator<T extends Validatable<unknown>> extends BaseValida
         return true;
     }
 
-    maxLength(limit: number) {
-        return new ArrayOfValidator(this.validator, {
-            ...this.options,
-            maxLength: Math.min(limit, this.options.maxLength),
-        });
+    minLength(limit: number): ArrayOfValidator<T> {
+        return new MinLengthArrayOfValidator(this, this.validator, limit);
     }
 
-    minLength(limit: number) {
-        return new ArrayOfValidator(this.validator, {
-            ...this.options,
-            minLength: Math.max(limit, this.options.minLength),
-        });
+    maxLength(limit: number): ArrayOfValidator<T> {
+        return new MaxLengthArrayOfValidator(this, this.validator, limit);
+    }
+}
+
+class MaxLengthArrayOfValidator<
+    T extends Validatable<unknown>,
+    V extends ArrayOfValidator<T>
+> extends ArrayOfValidator<T> {
+    constructor(
+        private readonly wrappedArrayOfValidator: V,
+        validator: T,
+        private readonly limit: number
+    ) {
+        super(validator);
+    }
+
+    validate(subject: unknown, diagnostics?: ValidationDiagnostics): subject is ValidatableType<V> {
+        if (!isArray(subject)) {
+            if (diagnostics) {
+                Object.assign(diagnostics, {
+                    error: 'not array',
+                });
+            }
+            return false;
+        }
+
+        if (subject.length > this.limit) {
+            if (diagnostics) {
+                Object.assign(diagnostics, {
+                    error: `array length exceeds maximum (${this.limit})`,
+                });
+            }
+            return false;
+        }
+
+        return this.wrappedArrayOfValidator.validate(subject, diagnostics);
+    }
+}
+
+class MinLengthArrayOfValidator<
+    T extends Validatable<unknown>,
+    V extends ArrayOfValidator<T>
+> extends ArrayOfValidator<T> {
+    constructor(
+        private readonly wrappedArrayOfValidator: V,
+        validator: T,
+        private readonly limit: number
+    ) {
+        super(validator);
+    }
+
+    validate(subject: unknown, diagnostics?: ValidationDiagnostics): subject is ValidatableType<V> {
+        if (!isArray(subject)) {
+            if (diagnostics) {
+                Object.assign(diagnostics, {
+                    error: 'not array',
+                });
+            }
+            return false;
+        }
+
+        if (subject.length < this.limit) {
+            if (diagnostics) {
+                Object.assign(diagnostics, {
+                    error: `array length below minimum (${this.limit})`,
+                });
+            }
+            return false;
+        }
+
+        return this.wrappedArrayOfValidator.validate(subject, diagnostics);
     }
 }
